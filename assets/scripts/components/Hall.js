853d5d01636c0b75245edf16ee8fc8cf7809f070
zengmengsi <873727356@qq.com>
2018-05-09 00:43:16 +0800

    mac updata

--- assets/scripts/components/Hall.js
+++ assets/scripts/components/Hall.js
@@ -4,19 +4,20 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        lblName:cc.Label,
-        lblMoney:cc.Label,
-        lblGems:cc.Label,
-        lblID:cc.Label,
-        lblNotice:cc.Label,
-        joinGameWin:cc.Node,
-        createRoomWin:cc.Node,
-        settingsWin:cc.Node,
-        helpWin:cc.Node,
-        xiaoxiWin:cc.Node,
-        btnJoinGame:cc.Node,
-        btnReturnGame:cc.Node,
-        sprHeadImg:cc.Sprite,
+        lblName: cc.Label,
+        lblMoney: cc.Label,
+        lblGems: cc.Label,
+        lblID: cc.Label,
+        lblNotice: cc.Label,
+        joinGameWin: cc.Node,
+        createRoomWin: cc.Node,
+        settingsWin: cc.Node,
+        helpWin: cc.Node,
+        xiaoxiWin: cc.Node,
+        feedbakcWin:cc.Node,
+        btnJoinGame: cc.Node,
+        btnReturnGame: cc.Node,
+        sprHeadImg: cc.Sprite,
         // foo: {
         //    default: null,
         //    url: cc.Texture2D,  // optional, default is typeof default
@@ -27,18 +28,18 @@ cc.Class({
         // },
         // ...
     },
-    
-    initNetHandlers:function(){
+
+    initNetHandlers: function() {
         var self = this;
     },
-    
-    onShare:function(){
-        cc.vv.anysdkMgr.share("天天麻将","天天麻将，包含了血战到底、血流成河等多种四川流行麻将玩法。");   
+
+    onShare: function() {
+        cc.vv.anysdkMgr.share("天天麻将", "天天麻将，包含了血战到底、血流成河等多种四川流行麻将玩法。");
     },
 
     // use this for initialization
-    onLoad: function () {
-        if(!cc.sys.isNative && cc.sys.isMobile){
+    onLoad: function() {
+        if (!cc.sys.isNative && cc.sys.isMobile) {
             var cvs = this.node.getComponent(cc.Canvas);
             cvs.fitHeight = true;
             cvs.fitWidth = true;
@@ -48,193 +49,202 @@ cc.Class({
         //     return;
         // }
         this.initLabels();
-        
-        if(cc.vv.gameNetMgr.roomId == null){
+
+        if (cc.vv.gameNetMgr.roomId == null) {
             this.btnJoinGame.active = true;
             this.btnReturnGame.active = false;
-        }
-        else{
+        } else {
             this.btnJoinGame.active = false;
             this.btnReturnGame.active = true;
         }
-        
+
         //var params = cc.vv.args;
-        var roomId = cc.vv.userMgr.oldRoomId 
-        if( roomId != null){
+        var roomId = cc.vv.userMgr.oldRoomId
+        if (roomId != null) {
             cc.vv.userMgr.oldRoomId = null;
             cc.vv.userMgr.enterRoom(roomId);
         }
-        
+
         var imgLoader = this.sprHeadImg.node.getComponent("ImageLoader");
         imgLoader.setUserID(cc.vv.userMgr.userId);
-        cc.vv.utils.addClickEvent(this.sprHeadImg.node,this.node,"Hall","onBtnClicked");
-        
+        cc.vv.utils.addClickEvent(this.sprHeadImg.node, this.node, "Hall", "onBtnClicked");
+
         this.addComponent("UserInfoShow");
-        
+
         this.initButtonHandler("Canvas/right_bottom/btn_shezhi");
         this.initButtonHandler("Canvas/right_bottom/btn_help");
         this.initButtonHandler("Canvas/right_bottom/btn_xiaoxi");
         this.helpWin.addComponent("OnBack");
         this.xiaoxiWin.addComponent("OnBack");
-        
-        if(!cc.vv.userMgr.notice){
+
+        if (!cc.vv.userMgr.notice) {
             cc.vv.userMgr.notice = {
-                version:null,
-                msg:"数据请求中...",
+                version: null,
+                msg: "数据请求中...",
             }
         }
-        
-        if(!cc.vv.userMgr.gemstip){
+
+        if (!cc.vv.userMgr.gemstip) {
             cc.vv.userMgr.gemstip = {
-                version:null,
-                msg:"数据请求中...",
+                version: null,
+                msg: "数据请求中...",
             }
         }
-        
+
         this.lblNotice.string = cc.vv.userMgr.notice.msg;
-        
+
         this.refreshInfo();
         this.refreshNotice();
         this.refreshGemsTip();
-        
+
         cc.vv.audioMgr.playBGM("bgMain.mp3");
 
         cc.vv.utils.addEscEvent(this.node);
 
     },
-    
-    refreshInfo:function(){
+
+    refreshInfo: function() {
         var self = this;
-        var onGet = function(ret){
-            if(ret.errcode !== 0){
+        var onGet = function(ret) {
+            if (ret.errcode !== 0) {
                 console.log(ret.errmsg);
-            }
-            else{
-                if(ret.gems != null){
-                    this.lblGems.string = ret.gems;    
+            } else {
+                if (ret.gems != null) {
+                    this.lblGems.string = ret.gems;
                 }
             }
         };
-        
+
         var data = {
-            account:cc.vv.userMgr.account,
-            sign:cc.vv.userMgr.sign,
+            account: cc.vv.userMgr.account,
+            sign: cc.vv.userMgr.sign,
         };
-        cc.vv.http.sendRequest("/get_user_status",data,onGet.bind(this));
+        cc.vv.http.sendRequest("/get_user_status", data, onGet.bind(this));
     },
-    
-    refreshGemsTip:function(){
+
+    refreshGemsTip: function() {
         var self = this;
-        var onGet = function(ret){
-            if(ret.errcode !== 0){
+        var onGet = function(ret) {
+            if (ret.errcode !== 0) {
                 console.log(ret.errmsg);
-            }
-            else{
+            } else {
                 cc.vv.userMgr.gemstip.version = ret.version;
-                cc.vv.userMgr.gemstip.msg = ret.msg.replace("<newline>","\n");
+                cc.vv.userMgr.gemstip.msg = ret.msg.replace("<newline>", "\n");
             }
         };
-        
+
         var data = {
-            account:cc.vv.userMgr.account,
-            sign:cc.vv.userMgr.sign,
-            type:"fkgm",
-            version:cc.vv.userMgr.gemstip.version
+            account: cc.vv.userMgr.account,
+            sign: cc.vv.userMgr.sign,
+            type: "fkgm",
+            version: cc.vv.userMgr.gemstip.version
         };
-        cc.vv.http.sendRequest("/get_message",data,onGet.bind(this));
+        cc.vv.http.sendRequest("/get_message", data, onGet.bind(this));
     },
-    
-    refreshNotice:function(){
+
+    refreshNotice: function() {
         var self = this;
-        var onGet = function(ret){
-            if(ret.errcode !== 0){
+        var onGet = function(ret) {
+            if (ret.errcode !== 0) {
                 console.log(ret.errmsg);
-            }
-            else{
+            } else {
                 cc.vv.userMgr.notice.version = ret.version;
                 cc.vv.userMgr.notice.msg = ret.msg;
                 this.lblNotice.string = ret.msg;
             }
         };
-        
+
         var data = {
-            account:cc.vv.userMgr.account,
-            sign:cc.vv.userMgr.sign,
-            type:"notice",
-            version:cc.vv.userMgr.notice.version
+            account: cc.vv.userMgr.account,
+            sign: cc.vv.userMgr.sign,
+            type: "notice",
+            version: cc.vv.userMgr.notice.version
         };
-        cc.vv.http.sendRequest("/get_message",data,onGet.bind(this));
+        cc.vv.http.sendRequest("/get_message", data, onGet.bind(this));
     },
-    
-    initButtonHandler:function(btnPath){
+
+    initButtonHandler: function(btnPath) {
         var btn = cc.find(btnPath);
-        cc.vv.utils.addClickEvent(btn,this.node,"Hall","onBtnClicked");        
+        cc.vv.utils.addClickEvent(btn, this.node, "Hall", "onBtnClicked");
     },
-    
-    
-    
-    initLabels:function(){
+
+
+
+    initLabels: function() {
         this.lblName.string = cc.vv.userMgr.userName;
         this.lblMoney.string = cc.vv.userMgr.coins;
         this.lblGems.string = cc.vv.userMgr.gems;
         this.lblID.string = "ID:" + cc.vv.userMgr.userId;
     },
-    
-    onBtnClicked:function(event){
-        if(event.target.name == "btn_shezhi"){
-            this.settingsWin.active = true;
-        }   
-        else if(event.target.name == "btn_help"){
-            this.helpWin.active = true;
+
+    onBtnClicked: function(event) {
+        console.log(event.target);
+        switch (event.target.name) {
+            case 'btn_shezhi':
+                this.settingsWin.active = true;
+                break;
+            case 'btn_help':
+                this.helpWin.active = true;
+                break;
+            case 'btn_xiaoxi':
+                this.xiaoxiWin.active = true;
+                break;
+            case 'btn_feedback':
+                this.feedbakcWin.active = true;
+                break;
+            case 'head':
+                cc.vv.userinfoShow.show(cc.vv.userMgr.userNacreateroleme, cc.vv.userMgr.userId, this.sprHeadImg, cc.vv.userMgr.sex, cc.vv.userMgr.ip);
+                break;
+            default:
+                console.log(event.target.name);
+                break;
         }
-        else if(event.target.name == "btn_xiaoxi"){
-            this.xiaoxiWin.active = true;
-        }
-        else if(event.target.name == "head"){
-            cc.vv.userinfoShow.show(cc.vv.userMgr.userName,cc.vv.userMgr.userId,this.sprHeadImg,cc.vv.userMgr.sex,cc.vv.userMgr.ip);
-        }
+        // if (event.target.name == "btn_shezhi") {
+        //     this.settingsWin.active = true;
+        // } else if (event.target.name == "btn_help") {
+        //     this.helpWin.active = true;
+        // } else if (event.target.name == "btn_xiaoxi") {
+        //     this.xiaoxiWin.active = true;
+        // } else if (event.target.name == "head") {
+        //     cc.vv.userinfoShow.show(cc.vv.userMgr.userName, cc.vv.userMgr.userId, this.sprHeadImg, cc.vv.userMgr.sex, cc.vv.userMgr.ip);
+        // }
     },
-    
-    onJoinGameClicked:function(){
+
+    onJoinGameClicked: function() {
         this.joinGameWin.active = true;
     },
-    
-    onReturnGameClicked:function(){
-        cc.vv.wc.show('正在返回游戏房间');
-        cc.director.loadScene("mjgame");  
+
+    onReturnGameClicked: function() {
+        // cc.vv.wc.show('正在返回游戏房间');
+        cc.director.loadScene("mjgame");
     },
-    
-    onBtnAddGemsClicked:function(){
-        cc.vv.alert.show("提示",cc.vv.userMgr.gemstip.msg,function(){
+
+    onBtnAddGemsClicked: function() {
+        cc.vv.alert.show("提示", cc.vv.userMgr.gemstip.msg, function() {
             this.onBtnTaobaoClicked();
         }.bind(this));
         this.refreshInfo();
     },
-    
-    onCreateRoomClicked:function(){
-        if(cc.vv.gameNetMgr.roomId != null){
-            cc.vv.alert.show("提示","房间已经创建!\n必须解散当前房间才能创建新的房间");
+
+    onCreateRoomClicked: function() {
+        if (cc.vv.gameNetMgr.roomId != null) {
+            cc.vv.alert.show("提示", "房间已经创建!\n必须解散当前房间才能创建新的房间");
             return;
         }
         console.log("onCreateRoomClicked");
-        this.createRoomWin.active = true;   
-    },
-    
-    onBtnTaobaoClicked:function(){
-        console.log("onFankuiClicked");
-        // cc.sys.openURL('https://shop596732896.taobao.com/');
+        this.createRoomWin.active = true;
     },
 
     // called every frame, uncomment this function to activate update callback
-    update: function (dt) {
+    update: function(dt) {
         var x = this.lblNotice.node.x;
-        x -= dt*100;
-        if(x + this.lblNotice.node.width < -1000){
+        x -= dt * 100;
+        if (x + this.lblNotice.node.width < -1000) {
             x = 500;
         }
         this.lblNotice.node.x = x;
-        
-        if(cc.vv && cc.vv.userMgr.roomData != null){
+
+        if (cc.vv && cc.vv.userMgr.roomData != null) {
             cc.vv.userMgr.enterRoom(cc.vv.userMgr.roomData);
             cc.vv.userMgr.roomData = null;
         }
@@ -242,8 +252,8 @@ cc.Class({
     /**
      * 加载图片
      */
-    addSpritePic: function(container,addres){
-        cc.loader.loadRes(addres, cc.SpriteFrame, function (err, spFrame) {
+    addSpritePic: function(container, addres) {
+        cc.loader.loadRes(addres, cc.SpriteFrame, function(err, spFrame) {
             container.spriteFrame = spFrame
         });
     }
